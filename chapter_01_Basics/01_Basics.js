e8d811efb04f20874dd4e9b6b500571aeb367f34
SaiKrishna7466 <krishnasai7466@gmail.com>
2026-05-11 16:59:32 +0530

    Initial commit of PlayWright learning code

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,4 @@
+console.log("Hello welcome to JavaScript basics!");
+
+let a=20;
+console.log(a);
---
+++ chapter_01_Basics/03_HotCode.js
@@ -0,0 +1,13 @@
+console.log("Hotcode Example");
+
+function add(a,b) {
+    return a+b;
+}
+
+let result;
+
+for(let i=0; i<10000; i++) {
+    result = add(i,i+1);
+}
+
+console.log("After 10000 iterations, result is : ", result);
