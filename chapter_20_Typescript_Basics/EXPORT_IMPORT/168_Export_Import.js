1087e6225369dd3bbf5c41768d611e472a74f50c
saikrishna7466 <krishnasai7466@gmail.com>
2026-06-30 14:36:31 +0530

    Typescript basics

---
+++ chapter_20_Typescript_Basics/EXPORT_IMPORT/168_Export_Import.js
@@ -0,0 +1,6 @@
+import { BASE_URL, formatUpperCaseString,fname } from "../testutils.js";
+
+console.log(BASE_URL);
+let result = formatUpperCaseString("Krishna Reddy");
+console.log(result);
+console.log(fname);
---
+++ chapter_20_Typescript_Basics/testutils.js
@@ -0,0 +1,7 @@
+export let BASE_URL = "https://app.vwo.com";
+
+export function formatUpperCaseString(name) {
+    return name.toUpperCase();
+}
+
+export let fname = "Krishna";
